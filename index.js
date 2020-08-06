af305290e6e9028679066f9731d2dc623bbd838d
Satoshi Nakagawa <ghnacker@gmail.com>
2020-08-06 19:46:59 +0900

    top: boolean | string

--- index.js
+++ index.js
@@ -14,15 +14,16 @@ const binLinks = opts => {
   // non-global top pkgs don't have any bins or mans linked.  From here on
   // out, if it's top, we know that it's global, so no need to pass that
   // option further down the stack.
-  if (top && !global)
+  const istop = typeof top !== 'string' && top;
+  if (istop && !global)
     return Promise.resolve()
 
   return Promise.all([
     // allow clobbering within the local node_modules/.bin folder.
     // only global bins are protected in this way, or else it is
     // yet another vector for excessive dependency conflicts.
-    linkBins({path, pkg, top, force: force || !top}),
-    linkMans({path, pkg, top, force}),
+    linkBins({path, pkg, top, force: force || !istop}),
+    linkMans({path, pkg, top: istop, force}),
   ])
 }
 
